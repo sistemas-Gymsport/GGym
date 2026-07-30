afd7c79b9b7a60b21630bd0d7a15db4047039207
Ángel López Martínez <angelserilopezmartinez@gmail.com>
2026-07-30 11:23:41 -0600

    commit fin v1.1.17

--- api/branches.js
+++ api/branches.js
@@ -28,6 +28,6 @@ export default async function handler(req, res) {
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error connecting to VPS flow' });
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 }
